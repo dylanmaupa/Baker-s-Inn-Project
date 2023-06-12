16817be6428913468ce266f4cee06a6fb108ebf6
dylanmaupa <114060288+dylanmaupa@users.noreply.github.com>
2023-06-12 16:02:52 +0200

    #

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,11 +13,11 @@ const Navbar = () => {
                 <img src={logo} alt="baker's_inn logo" className='logo' />
             </Link>
             <ul className={menuOpen ? "menu menuActive": "menu"}>
-                <li className="menu-item"><NavLink to="/about">About Us</NavLink></li>
-                <li className="menu-item"><NavLink to="/products">Products</NavLink></li>
-                <li className="menu-item"><NavLink to="/recipes">Recipes</NavLink></li>
-                <li className="menu-item"><NavLink to="/kids">Kid's Corner</NavLink></li>
-                <Button toPath='/contact' title="Contact Us" />
+                <li className="menu-item"><NavLink onClick={() => setMenuOpen(false)} to="/about">About Us</NavLink></li>
+                <li className="menu-item"><NavLink onClick={() => setMenuOpen(false)} to="/products">Products</NavLink></li>
+                <li className="menu-item"><NavLink onClick={() => setMenuOpen(false)} to="/recipes">Recipes</NavLink></li>
+                <li className="menu-item"><NavLink onClick={() => setMenuOpen(false)} to="/kids">Kid's Corner</NavLink></li>
+                <Button onClick={() => setMenuOpen(false)} toPath='/contact' title="Contact Us" />
             </ul>
             <div onClick={() => setMenuOpen(!menuOpen)} className={menuOpen ? "hamburger hamburgerActive" : "hamburger"}>
                 <div className="box"></div>
